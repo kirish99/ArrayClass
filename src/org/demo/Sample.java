47234e2464e8985e2daddf365143c5e5204da6ac
kirish99 <kirish7899@gmail.com>
2023-11-02 15:21:26 +0530

    array

---
+++ src/org/demo/Sample.java
@@ -0,0 +1,23 @@
+package org.demo;
+
+public class Sample {
+
+	public static void main(String[]args) {
+		
+	
+		int a[]=new int[5];
+		a[0]=10;
+		a[0]=10;
+		a[1]=20;
+		a[2]=50;
+		a[3]=60;
+		a[4]=80;
+		a[3]=60;
+		a[2]=50;
+		
+		for (int i = 0; i < a.length; i++) {
+			System.out.println(a[i]);
+		}
+		
+	}
+}
